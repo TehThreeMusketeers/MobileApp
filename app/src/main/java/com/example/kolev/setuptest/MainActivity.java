248afbe48fd1269992de01958b24fbc3b97cc206
Viktor Kolev <kolev519@gmail.com>
2018-01-30 18:45:31 +0000

    initial commit

---
+++ app/src/main/java/com/example/kolev/setuptest/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.kolev.setuptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import io.particle.android.sdk.cloud.ParticleCloudSDK;
+import io.particle.android.sdk.devicesetup.ParticleDeviceSetupLibrary;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ParticleDeviceSetupLibrary.init(this.getApplicationContext());
+
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the SetupDevice button */
+    public void onSetupDevice(View view) {
+        Log.d("MainActivity", "onSetupDevice called");
+        ParticleCloudSDK.getCloud().setAccessToken("fcdcd6d331480f5039f926248ede06b989069fb3");
+
+        System.out.println("ACCESS TOKEN IS" +ParticleCloudSDK.getCloud().getAccessToken());
+
+        ParticleDeviceSetupLibrary.startDeviceSetup(this, MainActivity.class);
+    }
+}
