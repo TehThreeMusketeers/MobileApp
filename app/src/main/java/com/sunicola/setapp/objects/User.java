f8955dc474ca70e517f75e5e9d7b20558167b3a1
Orber Soares Bom Jesus <orbeijo@hotmail.com>
2018-02-10 17:18:00 +0000

    ALL Devices fragment added (Shows Device IDS for devices on this users account)
    APICalls now updates the photon list table, which can then be used to retrieve data from
    
    New User and Photon object created, with proper getter and setter methods
    
    MainActivity and LoginActivity were refactored

---
+++ app/src/main/java/com/sunicola/setapp/objects/User.java
@@ -0,0 +1,57 @@
+package com.sunicola.setapp.objects;
+
+/**
+ * Created by soaresbo on 09/02/2018.
+ */
+
+public class User {
+    String email, first_name, last_name, session_token;
+    String id, refresh_token, access_token;
+
+    public User(String first_name, String last_name, String email, String session_token) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.session_token = session_token;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getSession_token() {
+        return session_token;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public String getRefresh_token() {
+        return refresh_token;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public void setRefresh_token(String refresh_token) {
+        this.refresh_token = refresh_token;
+    }
+}
